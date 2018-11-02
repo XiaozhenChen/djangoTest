2948409d90fcc166e57d7f4465c02433f600b2d7
XiaozhenChen <skycxz@qq.com>
2018-11-02 15:49:44 +0800

    add search.py to response to user request

---
+++ search.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+ 
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+ 
+# 表单
+def search_form(request):
+    return render_to_response('search_form.html')
+ 
+# 接收请求数据
+def search(request):  
+    request.encoding='utf-8'
+    if 'q' in request.GET:
+        message = 'Your search items is : ' + request.GET['q']
+    else:
+        message = 'You summit an empty fourm'
+    return HttpResponse(message)
